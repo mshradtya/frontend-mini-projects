e93d2fe44a99d125334af09aa9a635b72bdeecc7
m1shraditya <admtheone196@gmail.com>
2022-09-21 12:28:41 +0530

    updated clock design

--- analog-clock/index.js
+++ analog-clock/index.js
@@ -4,7 +4,6 @@ const secondEl = document.querySelector(".second");
 
 function updateClock() {
     const currentDate = new Date();
-    // setTimeout(updateClock, 1000);
     const hour = currentDate.getHours();
     const minute = currentDate.getMinutes();
     const second = currentDate.getSeconds();
@@ -15,7 +14,5 @@ function updateClock() {
     const secondDeg = (second / 60) * 360;
     secondEl.style.transform = `rotate(${secondDeg}deg)`;
 }
-
-// updateClock();
 
 setInterval(updateClock, 1000);
